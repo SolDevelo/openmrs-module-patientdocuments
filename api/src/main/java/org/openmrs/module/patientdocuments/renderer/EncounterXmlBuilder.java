7ed40921f27c4c569f94a3b28939c51287c26e67
druchniewicz <druchniewicz@soldevelo.com>
2026-04-15 10:21:19 +0200

    O3-5452: Fixed displaying # char in footer date

--- api/src/main/java/org/openmrs/module/patientdocuments/renderer/EncounterXmlBuilder.java
+++ api/src/main/java/org/openmrs/module/patientdocuments/renderer/EncounterXmlBuilder.java
@@ -234,7 +234,8 @@ public class EncounterXmlBuilder {
 
 		String userName = (user != null && user.getPersonName() != null) ? user.getPersonName().getFullName() : "System";
 		String systemId = (user != null && user.getSystemId() != null) ? user.getSystemId() : "Unknown";
-		String printTimestamp = OpenmrsUtil.getDateTimeFormat(locale).format(new Date());
+		String printTimestamp = OpenmrsUtil.getDateTimeFormat(locale).format(new Date())
+				.replace('\u202F', ' ').replace('\u00A0', ' ');
 
 		String printedBy = String.format("Printed by %s (%s) at %s", userName, systemId, printTimestamp);
 		xml.append("<printedBy>").append(escape(printedBy)).append("</printedBy>");
